09f93fb299fdc105d9698fb5dd890cdc83ed611b
celestecamp <celestecampm@gmail.com>
2026-06-06 19:49:46 -0300

    3 y 4

--- 04-trabajoPractico-Colas/tp_colas.c
+++ 04-trabajoPractico-Colas/tp_colas.c
@@ -44,12 +44,82 @@ Cola c_ej2_invertir(Cola c){
 
 // EJERCICIO 3:
 bool c_ej3_iguales(Cola c1, Cola c2){
+    Cola c_aux = c_crear();
+    int len1 = 0;
+    int len2 = 0;
+    bool iguales = true;
+	
+    while (!c_es_vacia(c1)) {
+        c_encolar(c_aux, c_desencolar(c1));
+        len1++;
+    }
+    
+    while (!c_es_vacia(c_aux)) {
+        c_encolar(c1, c_desencolar(c_aux));
+    }
 
+    
+    while (!c_es_vacia(c2)) {
+        c_encolar(c_aux, c_desencolar(c2));
+        len2++;
+    }
+    
+    while (!c_es_vacia(c_aux)) {
+        c_encolar(c2, c_desencolar(c_aux));
+    }
+ 
+    if (len1 != len2) {
+        return false; 
+    }
+
+    
+    for (int i = 0; i < len1; i++) {
+        TipoElemento e1 = c_desencolar(c1);
+        TipoElemento e2 = c_desencolar(c2);
+
+        if (e1->clave != e2->clave) {
+            iguales = false;
+        }
+        c_encolar(c1, e1);
+        c_encolar(c2, e2);
+    }
+
+    return iguales;
 }
 
 // EJERCICIO 4:
 Cola  c_ej4_colanorepetidos(Cola c){
+    Cola resultado = c_crear();
+    if (c_es_vacia(c)) {
+        return resultado;
+    }
+    int n = 0;
+    Cola c_aux = c_crear();
+    while (!c_es_vacia(c)) {
+        c_encolar(c_aux, c_desencolar(c));
+        n++;
+    }
+    while (!c_es_vacia(c_aux)) {
+        c_encolar(c, c_desencolar(c_aux));
+    }
+    for (int i = 0; i < n; i++) {
 
+        TipoElemento actual = c_desencolar(c);
+        int repeticiones = 1; 
+        for (int j = 0; j < n - 1; j++) {
+            TipoElemento temp = c_desencolar(c);
+            if (temp->clave == actual->clave) {
+                repeticiones++;
+            }
+            c_encolar(c, temp); 
+        }
+        if (repeticiones == 1) {
+            TipoElemento nuevo = te_crear_con_valor(actual->clave, actual->valor);
+            c_encolar(resultado, nuevo);
+        }
+        c_encolar(c, actual);
+    }
+    return resultado;
 }
 
 // EJERCICIO 5:
